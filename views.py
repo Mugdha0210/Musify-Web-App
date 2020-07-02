b0ee977dac823292063d86ff16592732b4f2b409
Mugdha0210 <63491406+Mugdha0210@users.noreply.github.com>
2020-07-02 22:01:58 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import SpotifyAPI
@@ -11,30 +12,45 @@ def index(request):
 	#context = { 'latest_question_list' : "Hello World", }
 	#print(request.POST)
 	return render(request, 'Musify/index.html')
-
-def search_page(request):
-	template = loader.get_template('Musify/searchpage.html')
-	#artist = request.POST['artist_name']
-	#genre = request.POST['genre_name']
-	#print(artist, genre)
-	if request.method == 'POST':
-		a_form = ArtistForm(request.POST)
-		if a_form.is_valid():
-			artist = a_form.cleaned_data.get("artist")
-			a = {"artist" : artist}
-			print(a)
-	else :
-		a_form = ArtistForm()
-	context = {'a_form' : a_form}
-	#display_page(request, context)
-	return render(request, 'Musify/searchpage.html', context)
 	
-def display_page(request, context):
+	
+def display_page(request, a):
 	client_id = 'e1026387021c413786266d809e931ca1'
 	client_secret = '6a3eb258f5b54b358860a5de726d0063'
 	spotify = SpotifyAPI(client_id, client_secret)
 	#template = loader.get_template('Musify/displaypage.html')
-	return render(request, 'Musify/displaypage.html', context)
+	r = spotify.get_artist(a["artist"])
+	image_url = r['images'][0]['url']
+	print(image_url)
+	#This image url should be used to display image on webpage
+	return render(request, 'Musify/displaypage.html', a)
+
+def search_page(request):
+    template = loader.get_template('Musify/searchpage.html')
+    #artist = request.POST['artist_name']
+    #genre = request.POST['genre_name']
+    #print(artist, genre)
+    if request.method == 'POST':
+        a_form = ArtistForm(request.POST)
+        if a_form.is_valid():
+            artist = a_form.cleaned_data.get("artist")
+            a = {"artist" : artist}
+            client_id = 'e1026387021c413786266d809e931ca1'
+            client_secret = '6a3eb258f5b54b358860a5de726d0063'
+            spotify = SpotifyAPI(client_id, client_secret)
+            r = spotify.get_artist(a["artist"])
+            image_url = r['images'][0]['url']
+            a = {"image_url" : image_url}
+            print(image_url)
+            #This image url should be used to display image on webpage
+            return render(request, 'Musify/displaypage.html', a)
+            print(a)
+
+    else :
+        a_form = ArtistForm()
+    context = {'a_form' : a_form}
+    #display_page(request, context)
+    return render(request, 'Musify/searchpage.html', context)
 	
 def redirect_page(request):
 	#template = loader.get_template('Musify/callback.html')
